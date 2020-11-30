adeb793e52dc012f0ea002b85daeb35a11020f90
下田　佳紀 <y.s@platinum.local>
2020-11-30 17:49:35 +0900

    strong parameter整備

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,13 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :detail, :priority, :situation, :deadline)
+  end
 end
