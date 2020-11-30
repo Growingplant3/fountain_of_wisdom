4a0b002f9a826781caefd61c64a9d0316e7a5bea
下田　佳紀 <y.s@platinum.local>
2020-11-30 14:33:16 +0900

    カラム制約の付与

--- db/migrate/20201130051744_create_tasks.rb
+++ db/migrate/20201130051744_create_tasks.rb
@@ -1,11 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, unique: true
       t.text :detail
-      t.integer :priority
-      t.integer :situation
-      t.time :deadline
+      t.integer :priority, null: false, default: 0
+      t.integer :situation, null: false, default: 0
+      t.time :deadline, null: false
 
       t.timestamps
     end
