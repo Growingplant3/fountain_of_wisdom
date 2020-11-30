938e39594e68b056cfed1b0783749fc083d28a1d
下田　佳紀 <y.s@platinum.local>
2020-11-30 23:31:50 +0900

    enumをモデルに記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,16 @@ class Task < ApplicationRecord
     uniqueness: true
   validates :priority, :situation, :deadline,
     presence: true
+
+  enum priority: {
+    unknown: 0,
+    low: 1,
+    middle: 2,
+    hight: 3
+  }
+  enum situation: {
+    stand_by: 0,
+    started: 1,
+    finished: 2
+  }
 end
