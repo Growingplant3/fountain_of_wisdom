edd1b7b944f031dfbf01c0d3ede1b05e974c7cb0
下田　佳紀 <y.s@platinum.local>
2020-12-01 14:42:10 +0900

    rspecの環境設定

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module FountainOfWisdom
     config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets false
+      g.test_framework :rspec,
+        model_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
     end
     config.generators.system_tests = nil
   end
