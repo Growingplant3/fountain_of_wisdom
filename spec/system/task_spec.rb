9f3e145fae53f47717e1a8eb0cb441b9baaf0e34
下田　佳紀 <y.s@platinum.local>
2020-12-02 21:52:05 +0900

    テストコードの修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in "task_name", with: "忘年会"
-        fill_in "task_detail", with: "今年最後の大宴会"
+        fill_in "task_name", with: "正しいタスク名"
+        fill_in "task_detail", with: "正しいタスク詳細"
         fill_in "task_deadline", with: "#{DateTime.now + 1.day}"
         click_on "登録する"
-        expect(page).to have_content "今年最後の大宴会" && "忘年会"
+        expect(page.text).to include "正しいタスク名" && "正しいタスク詳細"
       end
     end
   end
@@ -21,13 +21,20 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
+      before {
         task0 = task
         task1 = FactoryBot.create(:task, name: "正しいデータ10001")
         task2 = FactoryBot.create(:task, name: "正しいデータ10002")
+      }
+      it '新しいタスクが一番上に表示される' do
         visit tasks_path
-        task_first = first('.task_row')
-        expect(task_first.text).to include "正しいデータ10002"
+        task_list = all('.task_row')
+        expect(task_list.first.text).to include "正しいデータ10002"
+      end
+      it '古いタスクが一番下に表示される' do
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list.last.text).to include "正しいデータ10000"
       end
     end
   end
