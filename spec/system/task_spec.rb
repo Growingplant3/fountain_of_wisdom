afd6d30aa4dd794ad85c02026685369df735020f
下田　佳紀 <y.s@platinum.local>
2020-12-02 14:52:07 +0900

    降順テスト追記

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let(:task) { FactoryBot.create(:task) }
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -15,16 +16,24 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task)
         visit tasks_path
         expect(page).to have_content "タスク一覧"
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task0 = task
+        task1 = FactoryBot.create(:task, name: "正しいデータ10001")
+        task2 = FactoryBot.create(:task, name: "正しいデータ10002")
+        visit tasks_path
+        task_first = first('.task_row')
+        expect(task_first.text.slice(5..15)).to eq "正しいデータ10002"
       end
     end
   end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task)
         visit task_path(task)
         expect(page).to have_content "タスクの詳細"
       end
