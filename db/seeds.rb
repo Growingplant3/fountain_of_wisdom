5587c67b048e4e112624e89f30cbc9b3f1c819b4
下田　佳紀 <y.s@platinum.local>
2020-12-05 00:48:27 +0900

    seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-alphabet_array = ("a".."z").to_a
-26.times do |index|
-  name = alphabet_array.pop
+alphabet_array = ("A".."z").to_a
+58.times do |index|
+  name = "task " + alphabet_array.pop
   priority = Random.rand(0..2)
   situation = Random.rand(0..2)
-  deadline = DateTime.now + Random.rand(1..30).day
+  deadline = DateTime.now + Random.rand(30..50).day
   Task.create(name: name, detail: index, priority: priority, situation: situation, deadline: deadline)
 end
