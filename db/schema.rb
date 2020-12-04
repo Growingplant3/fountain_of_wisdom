5d37696a3fcf18287625422ab56fee57b4320afb
下田　佳紀 <y.s@platinum.local>
2020-12-04 11:14:38 +0900

    検索インデックス付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_051744) do
+ActiveRecord::Schema.define(version: 2020_12_04_021329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_051744) do
     t.datetime "deadline", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
 end
