236b883a84fef207da74f18b658100b3b6376282
下田　佳紀 <y.s@platinum.local>
2020-12-05 00:37:40 +0900

    seedデータを用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+alphabet_array = ("a".."z").to_a
+26.times do |index|
+  name = alphabet_array.pop
+  priority = Random.rand(0..2)
+  situation = Random.rand(0..2)
+  deadline = DateTime.now + Random.rand(1..30).day
+  Task.create(name: name, detail: index, priority: priority, situation: situation, deadline: deadline)
+end
