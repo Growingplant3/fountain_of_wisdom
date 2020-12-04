a4dfae12529590ace1febba256f9d21c04ab7a17
下田　佳紀 <y.s@platinum.local>
2020-12-04 23:55:45 +0900

    モデル修正によるテストコード修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,25 +15,25 @@ RSpec.describe 'タスクモデル機能', type: :model do
     end
     context 'タスクの重要度が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: "新年会", detail: nil, priority: nil, situation: 0, deadline: DateTime.tomorrow)
+        task = Task.new(name: "失敗テスト", detail: nil, priority: nil, situation: 0, deadline: DateTime.tomorrow)
         expect(task).not_to be_valid
       end
     end
     context 'タスクの進行状況が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: "新年会", detail: nil, priority: 0, situation: nil, deadline: DateTime.tomorrow)
+        task = Task.new(name: "失敗テスト", detail: nil, priority: 0, situation: nil, deadline: DateTime.tomorrow)
         expect(task).not_to be_valid
       end
     end
     context 'タスクの終了期限が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: "新年会", detail: nil, priority: 0, situation: 0, deadline: nil)
+        task = Task.new(name: "失敗テスト", detail: nil, priority: 0, situation: 0, deadline: nil)
         expect(task).not_to be_valid
       end
     end
     context 'タスク名とタスクの重要度とタスクの進行状況とタスクの終了期限が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(name: "結婚式", detail: nil, priority: 3, situation: 2, deadline: DateTime.tomorrow)
+        task = Task.new(name: "成功テスト", detail: nil, priority: 2, situation: 2, deadline: DateTime.tomorrow)
         expect(task).to be_valid
       end
     end
