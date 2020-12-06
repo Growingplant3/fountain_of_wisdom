4090f58b6217c9b6f7e58bc52ce4fa2df6f8b4b5
下田　佳紀 <y.s@platinum.local>
2020-12-06 20:11:13 +0900

    ヘルパーメソッドの移植と拡張

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def change_root
+    case action_name
+    when "new"
+      tasks_path if controller_name == "tasks"
+      users_path if controller_name == "users"
+    when "edit"
+      task_path if controller_name == "tasks"
+      user_path if controller_name == "users"
+    end
+  end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,10 +1,2 @@
 module TasksHelper
-  def change_root
-    case action_name
-    when "new"
-      tasks_path
-    when "edit"
-      task_path
-    end
-  end
 end
