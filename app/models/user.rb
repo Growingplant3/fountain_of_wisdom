fb1ca1b8ec71f45eb2c2b604734acab7960b38f9
下田　佳紀 <y.s@platinum.local>
2020-12-06 18:37:44 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,15 @@ class User < ApplicationRecord
 
   validates :name,
     presence: true,
-    uniqueness: true
+    uniqueness: true,
     length: { maximum: 20 }
   validates :email, 
     presence: true,
     uniqueness: true,
     length: { maximum: 50 },
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password
+  validates :password,
     presence: true,
     length: { in: 8..16 },
-    format: { with: /\A[a-z\d@#$_!?]{8,16}\z/ }
-  validate :do_not_accept_without_password_format
-
-  def do_not_accept_without_password_format
-    errors.add(:password, I18n.t(errors.messages.need_password_format))
-  end
+    format: { with: /[a-z\d@#$_!?]{8,16}/, message: I18n.t("errors.messages.need_password_format") }
 end
