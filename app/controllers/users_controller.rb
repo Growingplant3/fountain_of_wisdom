e7f183953bef78f0dc9854fac83341522f9f90d3
下田　佳紀 <y.s@platinum.local>
2020-12-06 19:43:56 +0900

    showアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
   def new
     @user = User.new
   end
@@ -7,15 +9,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash.now[:notice] = "アカウントの作成に成功しました。"
-      render :new
+      redirect_to user_path(@user)
     else
       flash.now[:danger] = "アカウントの作成に失敗しました。"
       render :new
     end
   end
 
+  def show
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
