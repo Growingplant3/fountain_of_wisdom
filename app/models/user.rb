0d8efa0c77a1411f6a0eb1cd1374543c92387227
下田　佳紀 <y.s@platinum.local>
2020-12-06 16:14:53 +0900

    has_secure_password追加/バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_validation { email.downcase! }
+
+  validates :name,
+    presence: true,
+    uniqueness: true
+    length: { maximum: 20 }
+  validates :email, 
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 50 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password
+    presence: true,
+    length: { in: 8..16 },
+    format: { with: /\A[a-z\d@#$_!?]{8,16}\z/ }
+  validate :do_not_accept_without_password_format
+
+  def do_not_accept_without_password_format
+    errors.add(:password, I18n.t(errors.messages.need_password_format))
+  end
 end
