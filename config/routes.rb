e34e4f1d9b66e7915d4358b60e67f13e317df7c3
下田　佳紀 <y.s@platinum.local>
2020-12-07 01:13:56 +0900

    初期ルートを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: 'sessions#new'
   resources :tasks
   resources :users, except: [:index]
   resources :sessions, only: %i[new create destroy]
