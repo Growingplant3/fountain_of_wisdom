e88480897d341c3f10ca74b35e363fef9f57528c
下田　佳紀 <y.s@platinum.local>
2020-12-07 21:49:25 +0900

    userに関するアクセス制限記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :sign_in_check, only: %i[show edit update destroy]
 
   def new
     @user = User.new
