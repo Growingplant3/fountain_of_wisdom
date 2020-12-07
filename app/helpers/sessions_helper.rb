d0a6ec93b7627970e9d96e22a8ed7fe37bf619ad
下田　佳紀 <y.s@platinum.local>
2020-12-07 18:44:41 +0900

    非ログイン時にタスク機能使えない

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,13 @@ module SessionsHelper
   def logged_in?
     current_user.present?
   end
+
+  def sign_in_check
+    if logged_in?
+      return
+    else
+      flash[:danger] = "ログインする必要があります"
+      redirect_to new_session_path
+    end
+  end
 end
