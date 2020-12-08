04a899351b8fff20b33a50a1ee8de5799089bd20
下田　佳紀 <y.s@platinum.local>
2020-12-08 14:09:31 +0900

    admin用にメソッド拡張

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,11 @@ module ApplicationHelper
     when "new"
       tasks_path if controller_name == "tasks"
       users_path if controller_name == "users"
+      admin_users_path if controller_name == "admin/users"
     when "edit"
       task_path if controller_name == "tasks"
       user_path if controller_name == "users"
+      admin_user_path if controller_name == "admin/users"
     end
   end
 end
