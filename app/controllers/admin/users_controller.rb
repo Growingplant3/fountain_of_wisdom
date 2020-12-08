8dee31c21a4346b3dbf21a597136fc766d19c02e
下田　佳紀 <y.s@platinum.local>
2020-12-08 11:37:13 +0900

    各種アクションをusers/controllerから移植し修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,53 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.all
   end
 
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.now[:notice] = "アカウントの作成に成功しました。"
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:danger] = "アカウントの作成に失敗しました。"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "アカウントの編集に成功しました。"
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:danger] = "アカウントの編集に失敗しました。"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:danger] = "アカウントを削除しました。"
+    redirect_to admin_users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
