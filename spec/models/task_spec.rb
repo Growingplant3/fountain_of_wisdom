3272fc00996a1d79b58df62f3e89c4849e1e63d2
下田　佳紀 <y.s@platinum.local>
2020-12-09 11:58:38 +0900

    userモデル追加によるテストコードの修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,55 +1,56 @@
 require 'rails_helper'
 RSpec.describe 'タスクモデル機能', type: :model do
+  let(:admin_user) { FactoryBot.create(:admin_user) }
   describe 'バリデーションのテスト' do
     context 'タスク名が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: nil, detail: "失敗テスト", priority: 0, situation: 0, deadline: DateTime.tomorrow)
+        task = admin_user.tasks.new(name: nil, detail: "失敗テスト", priority: 0, situation: 0, deadline: DateTime.tomorrow)
         expect(task).not_to be_valid
       end
     end
 
     context 'タスク詳細が空の場合' do
       it 'バリデーションにひっかからない' do
-        task = Task.new(name: "成功テスト", detail: nil, priority: 0, situation: 0, deadline: DateTime.tomorrow)
+        task = admin_user.tasks.new(name: "成功テスト", detail: nil, priority: 0, situation: 0, deadline: DateTime.tomorrow)
         expect(task).to be_valid
       end
     end
 
     context 'タスクの重要度が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: "失敗テスト", detail: nil, priority: nil, situation: 0, deadline: DateTime.tomorrow)
+        task = admin_user.tasks.new(name: "失敗テスト", detail: nil, priority: nil, situation: 0, deadline: DateTime.tomorrow)
         expect(task).not_to be_valid
       end
     end
 
     context 'タスクの進行状況が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: "失敗テスト", detail: nil, priority: 0, situation: nil, deadline: DateTime.tomorrow)
+        task = admin_user.tasks.new(name: "失敗テスト", detail: nil, priority: 0, situation: nil, deadline: DateTime.tomorrow)
         expect(task).not_to be_valid
       end
     end
 
     context 'タスクの終了期限が空の場合' do
       it 'バリデーションにひっかかる' do
-        task = Task.new(name: "失敗テスト", detail: nil, priority: 0, situation: 0, deadline: nil)
+        task = admin_user.tasks.new(name: "失敗テスト", detail: nil, priority: 0, situation: 0, deadline: nil)
         expect(task).not_to be_valid
       end
     end
 
     context 'タスク名とタスクの重要度とタスクの進行状況とタスクの終了期限が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(name: "成功テスト", detail: nil, priority: 2, situation: 2, deadline: DateTime.tomorrow)
+        task = admin_user.tasks.new(name: "成功テスト", detail: nil, priority: 2, situation: 2, deadline: DateTime.tomorrow)
         expect(task).to be_valid
       end
     end
   end
 
   describe '検索機能' do
-    let!(:task) { FactoryBot.create(:task) }
-    let!(:task_1) { FactoryBot.create(:task, name:"正しいデータ1", situation: 1) }
-    let!(:task_2) { FactoryBot.create(:task, name:"楽しい旅行データ0", situation: 0) }
-    let!(:task_3) { FactoryBot.create(:task, name:"楽しい旅行データ1", situation: 1) }
-    let!(:task_4) { FactoryBot.create(:task, name:"楽しいデータ0", situation: 2) }
+    let!(:task) { FactoryBot.create(:task, user_id: admin_user.id) }
+    let!(:task_1) { FactoryBot.create(:task, name:"正しいデータ1", situation: 1, user_id: admin_user.id) }
+    let!(:task_2) { FactoryBot.create(:task, name:"楽しい旅行データ0", situation: 0, user_id: admin_user.id) }
+    let!(:task_3) { FactoryBot.create(:task, name:"楽しい旅行データ1", situation: 1, user_id: admin_user.id) }
+    let!(:task_4) { FactoryBot.create(:task, name:"楽しいデータ0", situation: 2, user_id: admin_user.id) }
   
     context 'scopeメソッドでタイトルのあいまい検索をした場合' do
       it "検索キーワードを含むタスクが絞り込まれる" do
