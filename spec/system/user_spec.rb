b596fbe365721e27a98a6492b52fa0404fd9bd9d
下田　佳紀 <y.s@platinum.local>
2020-12-10 14:46:59 +0900

    アカウント削除/編集のテストコード

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -24,43 +24,36 @@ RSpec.describe 'ユーザー管理機能', type: :system do
     end
   end
 
-  # describe 'アカウント削除機能' do
-  #   # before {
-  #   #   general_user
-  #   #   visit new_session_path
-  #   #   fill_in "session_email", with: "general@example.com"
-  #   #   fill_in "session_password", with: "password"
-  #   #   click_on "ログインする", match: :first
-  #   # }
-  #   it 'アカウントを削除するボタンを押せばアカウントを削除できる' do
-  #     # visit user_path(general_user)
-  #     general_user
-  #     visit new_session_path
-  #     binding.pry
-  #     fill_in "session_email", with: "general@example.com"
-  #     fill_in "session_password", with: "password"
-  #     click_on "ログインする", match: :last
-  #     binding.pry
-  #     page.accept_confirm do
-  #       click_on "アカウントを削除する"
-  #     end
-  #     expect(page.text).to include "アカウントを削除しました。"
-  #   end
-  # end
+  describe 'アカウント削除機能' do
+    before {
+      general_user
+      visit new_session_path
+      fill_in "session_email", with: "general@example.com"
+      fill_in "session_password", with: "password"
+      find("#create_tag").click
+    }
+    it 'アカウントを削除するボタンを押せばアカウントを削除できる' do
+      page.accept_confirm do
+        click_on "アカウントを削除する"
+      end
+      expect(page.text).to include "アカウントを削除しました。"
+    end
+  end
     
-  # describe 'アカウント削除機能' do
-  #   before {
-  #     visit new_user_path
-  #     fill_in "user_name", with: "correct_user_name"
-  #     fill_in "user_email", with: "correct_user_email@example.com"
-  #     fill_in "user_password", with: "correct@password"
-  #     fill_in "user_password_confirmation", with: "correct@password"
-  #     click_on "アカウントを新規作成する"
-  #   }
-  #   it 'アカウントを編集するボタンを押せばアカウントを編集できる' do
-  #     click_on "アカウントを編集する"
-  #     fill_in "user_name", with: "edit_user_name"
-  #     fill_in "user"
-  #   end
-  # end
+  describe 'アカウント編集機能' do
+    before {
+      general_user
+      visit new_session_path
+      fill_in "session_email", with: "general@example.com"
+      fill_in "session_password", with: "password"
+      find("#create_tag").click
+    }
+    it 'アカウントを編集するボタンを押せばアカウントを編集できる' do
+      click_on "アカウントを編集する"
+      fill_in "user_name", with: "edit_user_name"
+      fill_in "user_email", with: "edit@example.com"
+      fill_in "user_password", with: "editpassword"
+      fill_in "user_password_confirmation", with: "editpassword"
+    end
+  end
 end
