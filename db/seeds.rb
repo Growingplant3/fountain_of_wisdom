4cbe8cd3750bf6d399cae6a69422b6a58373dfb2
下田　佳紀 <y.s@platinum.local>
2020-12-11 17:17:48 +0900

    作成ずみタスクに初期ユーザーを紐付け

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,25 @@
 # end
 
 # 195112件のシードデータが多すぎる場合にはこちらを使ってください。
-alphabet_array1 = ("A".."z").to_a
-alphabet_array2 = ("a".."z").to_a
-task_name_array = []
-alphabet_array1.each do |first_letter|
-  alphabet_array2.each do |second_letter|
-    task_name_array.push(first_letter + second_letter)
-  end
-end
+# alphabet_array1 = ("A".."z").to_a
+# alphabet_array2 = ("a".."z").to_a
+# task_name_array = []
+# alphabet_array1.each do |first_letter|
+#   alphabet_array2.each do |second_letter|
+#     task_name_array.push(first_letter + second_letter)
+#   end
+# end
 
-1508.times do |index|
-  name = "task " + task_name_array.pop
-  priority = Random.rand(0..2)
-  situation = Random.rand(0..2)
-  deadline = DateTime.now + Random.rand(30..50).day
-  Task.create(name: name, detail: index, priority: priority, situation: situation, deadline: deadline)
-end
+# 1508.times do |index|
+#   name = "task " + task_name_array.pop
+#   priority = Random.rand(0..2)
+#   situation = Random.rand(0..2)
+#   deadline = DateTime.now + Random.rand(30..50).day
+#   Task.create(name: name, detail: index, priority: priority, situation: situation, deadline: deadline)
+# end
 
-User.create(name: "fujisan", email: "fujisan@example.com")
+first_admin_user = User.create(name: "fujisan", email: "fujisan@example.com", password: "password", password_confirmation: "password", admin: "admin")
+
+Task.where(user_id: nil).each do |task|
+  task.update(user_id: first_admin_user.id)
+end
