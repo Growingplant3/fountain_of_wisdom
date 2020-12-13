a1da26f0db36e226072af8d45e1e8e8b0a64deb3
下田　佳紀 <y.s@platinum.local>
2020-12-13 21:08:21 +0900

    エラー処理用のメソッド整備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404
+    @error = true
+    render template: 'errors/error_404', status: 404, content_type: 'text/html'
+  end
+
+  def render_500
+    @error = true
+    render template: 'errors/error_500', status: 500, content_type: 'text/html'
+  end
 end
