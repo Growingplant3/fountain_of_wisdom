ab318de339c5b428ebcbca981c609dcc38e54747
下田　佳紀 <y.s@platinum.local>
2020-12-14 19:18:12 +0900

    index/destroyアクション整備

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,4 +1,10 @@
 class LabelsController < ApplicationController
+  before_action :set_label, only: %i[destroy]
+
+  def index
+    @labels = current_user.labels
+  end
+
   def new
     @label = current_user.labels.new
   end
@@ -7,11 +13,17 @@ class LabelsController < ApplicationController
     @label = current_user.labels.new(label_params)
     if @label.save
       flash[:notice] = "ラベルの登録に成功しました。"
-      redirect_to new_label_path
+      redirect_to label_path(@label)
     else
       flash.now[:danger] = "ラベルの登録に失敗しました。"
       render :new
     end
+  end
+
+  def destroy
+    @label.destroy
+    flash[:danger] = "ラベルの削除に成功しました。"
+    redirect_to labels_path
   end
 
   private
