2a927b6c33dd58eba0140993f356469b031c480a
下田　佳紀 <y.s@platinum.local>
2020-12-14 23:07:17 +0900

    検索用タスク追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     situation { 0 }
     deadline { "3000/01/01".to_datetime }  
   end
+
+  factory :another_task, class: Task do
+    name { "ラベル用タスク" }
+    detail { nil }
+    priority { 1 }
+    situation { 1 }
+    deadline { "4000/12/31".to_datetime}
+  end
 end
