b9aad2fb91422c8a58c61cf6c0eeed1cb920dfcc
下田　佳紀 <y.s@platinum.local>
2020-12-14 19:26:55 +0900

    ヘルパーの拡張

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,12 @@ module ApplicationHelper
       tasks_path if controller_name == "tasks"
       users_path if controller_name == "users"
       admin_users_path if controller_name == "admin/users"
+      labels_path if controller_name == "labels"
     when "edit"
       task_path if controller_name == "tasks"
       user_path if controller_name == "users"
       admin_user_path if controller_name == "admin/users"
+      label_path if controller_name == "labels"
     end
   end
 end
