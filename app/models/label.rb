137f9ec4c4bdee6e760ed23bc344dde72677727f
下田　佳紀 <y.s@platinum.local>
2020-12-14 21:11:32 +0900

    scope作成

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,4 +3,6 @@ class Label < ApplicationRecord
   has_many :label_tasks, dependent: :destroy
   has_many :tasks, through: :label_tasks, source: :task
   belongs_to :user
+
+  scope :label_value, -> (label_value) { where(name: label_value) }
 end
