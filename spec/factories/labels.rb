5babf5638bd20d525e47a110c7d87634d90c7341
下田　佳紀 <y.s@platinum.local>
2020-12-14 22:00:06 +0900

    初期データ追加

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -2,4 +2,12 @@ FactoryBot.define do
   factory :label do
     name { "正しいラベル" }
   end
+
+  factory :rare_label, class: Label do
+    name { "珍しいラベル" }
+  end
+
+  factory :common_label, class: Label do
+    name { "よくあるラベル" }
+  end
 end
