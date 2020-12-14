29155f7352215e7a7360fcbfa051df084cd0edae
下田　佳紀 <y.s@platinum.local>
2020-12-14 21:28:28 +0900

    ラベルのバリデーションのテストコード

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Label, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin_user) { FactoryBot.create(:admin_user) }
+  describe 'バリデーションのテスト' do
+    context 'ラベル名が空の場合' do
+      it 'バリデーションにひっかかる' do
+        label = admin_user.labels.new(name: nil)
+        expect(label).not_to be_valid
+      end
+    end
+
+    context 'ラベル名が空ではない場合' do
+      it 'バリデーションに引っかからない' do
+        label = admin_user.labels.new(name: "正しいラベル")
+        expect(label).to be_valid
+      end
+    end
+  end
 end
