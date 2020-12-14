a7e0b14648017004d8d6f4136a1770d290b38551
下田　佳紀 <y.s@platinum.local>
2020-12-14 18:46:26 +0900

    new/createアクション整備

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,25 @@
 class LabelsController < ApplicationController
+  def new
+    @label = current_user.labels.new
+  end
+
+  def create
+    @label = current_user.labels.new(label_params)
+    if @label.save
+      flash[:notice] = "ラベルの登録に成功しました。"
+      redirect_to new_label_path
+    else
+      flash.now[:danger] = "ラベルの登録に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+  def label_params
+    params.require(:label).permit(:name, :user_id)
+  end
+
+  def set_label
+    @label = Label.find(params[:id])
+  end
 end
